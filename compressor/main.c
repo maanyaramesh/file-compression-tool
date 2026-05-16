3b7e105df34f87e7c28e141ada0f81c1f72b8c78
Maanya Ramesh <maanyaramesh93@gmail.com>
2026-05-16 09:23:03 +0530

    Update main.c

--- compressor/main.c
+++ compressor/main.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+#include "utils.h"
 #include "rle.h"
+#include "huffman.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc != 5)
+    if (argc < 3)
     {
         printf("Usage:\n");
-        printf("./compressor [compress/decompress] [rle] input output\n");
+
+        printf("./compressor compress rle input output\n");
+
+        printf("./compressor huffman input\n");
+
         return 1;
     }
 
     char *operation = argv[1];
-    char *algorithm = argv[2];
 
-    if (strcmp(operation, "compress") == 0 &&
-        strcmp(algorithm, "rle") == 0)
+    if (strcmp(operation, "compress") == 0)
     {
-        rle_compress(argv[3], argv[4]);
+        if (strcmp(argv[2], "rle") == 0)
+        {
+            rle_compress(argv[3], argv[4]);
+            print_compression_stats(argv[3], argv[4]);
+        }
     }
-    else if (strcmp(operation, "decompress") == 0 &&
-             strcmp(algorithm, "rle") == 0)
+
+    else if (strcmp(operation, "decompress") == 0)
     {
-        rle_decompress(argv[3], argv[4]);
+        if (strcmp(argv[2], "rle") == 0)
+        {
+            rle_decompress(argv[3], argv[4]);
+        }
     }
+
+    else if (strcmp(operation, "huffman") == 0)
+    {
+        huffman_demo(argv[2]);
+    }
+
     else
     {
         printf("Invalid command\n");
