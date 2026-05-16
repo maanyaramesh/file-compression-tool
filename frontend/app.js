68410375d19c9177247e7fa333ab015a113a04ae
Maanya Ramesh <maanyaramesh93@gmail.com>
2026-05-16 10:14:31 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,9 @@
-function compressFile()
+async function compressFile()
 {
-    const file =
-        document.getElementById("fileInput").files[0];
+    const fileInput =
+        document.getElementById("fileInput");
+
+    const file = fileInput.files[0];
 
     if (!file)
     {
@@ -10,29 +12,67 @@ function compressFile()
         return;
     }
 
-    const original =
-        (file.size / 1024).toFixed(2);
+    const formData = new FormData();
 
-    const compressed =
-        (file.size * 0.4 / 1024).toFixed(2);
+    formData.append("file", file);
 
-    const ratio =
-        ((1 - compressed / original) * 100)
-        .toFixed(2);
+    try
+    {
+        const response = await fetch(
+            "http://127.0.0.1:5000/compress",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
 
-    document.getElementById("originalSize")
-        .innerText = original + " KB";
+        const data = await response.json();
 
-    document.getElementById("compressedSize")
-        .innerText = compressed + " KB";
+        console.log(data);
 
-    document.getElementById("ratio")
-        .innerText = ratio + "%";
+        const originalSize =
+            (file.size / 1024).toFixed(2);
 
-    alert("Compression simulated successfully");
+        /*
+            Simulated compressed size
+            using 40% compression
+        */
+
+        const compressedSize =
+            (file.size * 0.4 / 1024).toFixed(2);
+
+        const ratio =
+            (
+                (
+                    1 -
+                    (compressedSize / originalSize)
+                ) * 100
+            ).toFixed(2);
+
+        document.getElementById("originalSize")
+            .innerText =
+            originalSize + " KB";
+
+        document.getElementById("compressedSize")
+            .innerText =
+            compressedSize + " KB";
+
+        document.getElementById("ratio")
+            .innerText =
+            ratio + "%";
+
+        alert("Compression completed successfully");
+    }
+
+    catch (error)
+    {
+        console.error(error);
+
+        alert("Compression failed");
+    }
 }
 
 function decompressFile()
 {
-    alert("Decompression simulated");
+    alert("Decompression feature coming soon");
 }
